05ef1e631ad8d60a5f460402114c184d23b90780
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-07-17 21:49:01 +0300

    init

---
+++ src/main/java/com/bloghub/recommendation_module/RecommendationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.bloghub.recommendation_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloghub/recommendation_module/RecommendationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloghub.recommendation_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
